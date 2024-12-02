02fa0a114176711a8bcbafdcddbf879c2eea7ee2
Abhay-1718 <abhaysingh80766@gmail.com>
2024-12-02 11:28:50 +0530

    setup server.js file , change module in package.json file

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.get("/", (req,res) => {
+res.send("API working")
+})
+
+
+app.listen(port,  () => {
+    console.log("server running at port : " + port);
+    
+})
