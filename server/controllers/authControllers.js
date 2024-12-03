be1a380c80110f65ad0a6e6b1a684c506df55212
Abhay-1718 <abhaysingh80766@gmail.com>
2024-12-03 11:17:22 +0530

    create backend services for user auth login/register

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,93 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({
+        success: false,
+        message: "user already exist",
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "please enter a strong password",
+      });
+    }
+   //hasing user password
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save(); //save in db
+
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User Does not exist ",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+        const token = createToken(user._id)
+        res.json({ success: true, token });
+    }else{
+        res.json({
+            message: "Invalid credentials",
+            success: false,
+          });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { registerUser, loginUser}
--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"
-import connectDB from "./config/mongoDB.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/mongoDB.js";
+import authRoutes from "./routes/authRoutes.js";
 
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000
-connectDB()
-
-
+const port = process.env.PORT || 4000;
+connectDB();
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/auth", authRoutes);
 
-app.get("/", (req,res) => {
-res.send("API working")
-})
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-
-app.listen(port,  () => {
-    console.log("server running at port : " + port);
-    
-})
+app.listen(port, () => {
+  console.log("server running at port : " + port);
+});
